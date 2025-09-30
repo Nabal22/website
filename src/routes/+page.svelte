53fc3b2e1a4a9f3fccedadeb6f90ab4ac85bcb20
Alban Talagrand <alban.talagrand@gmail.com>
2025-09-30 23:11:35 +0200

    feat: ajout du super gif dj nabal7

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import djGif from '$lib/assets/nabal-dj.gif';
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen">
+  <img 
+    src={djGif} 
+    alt="Animated DJ mixing music"
+    class="w-72 h-72 object-contain"
+  />
+</div>
